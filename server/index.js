c30452f854e0f8f81341018fc4bb8c4a96e9d532
Raul <raul.alhena@gmail.com>
2023-12-14 12:27:27 +0100

    installed all needed modules & generated private and public keys

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0314574d28f76293d0abf3b8fbd558c631cacc5500658307ea3e64fdb177a8f961": 100,
+  "03d6708561325e9d5432418cb965bf41e2d14e3380f3a0055e0d15e0682052d98e": 50,
+  "026358092beccb5f7e35b07eee5495ca5b5f369c7e1bcc5c25e6e686ca0e6378b6": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
